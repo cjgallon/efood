c0b4f180106641b12594936ac58029abe362a458
Samir <samirperezb1004@gmail.com>
2023-05-07 17:57:31 -0500

    created UIController

---
+++ lib/controllers/ui_controller.dart
@@ -0,0 +1,9 @@
+import 'package:get/get.dart';
+
+class UIController extends GetxController {
+  bool quickProductsModalShown = false;
+
+  void closeQuickProductsModal() {
+    quickProductsModalShown = true;
+  }
+}
