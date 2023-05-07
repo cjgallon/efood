d27eb123b737afa205e438e918fa74a42e0a051c
Samir <samirperezb1004@gmail.com>
2023-05-07 15:21:38 -0500

    installed firebase

---
+++ lib/config/configuration.dart
@@ -0,0 +1,18 @@
+//create a file called configuration.dart on this folder
+// copy the values from firebase
+
+class Configuration {
+  static const apiKey =
+      "AIzaSyBucqFsYETDTwcRMT0USzzmPGrdN-Ls_Fo"; // tomado de current_key
+  static const authDomain =
+      "efood-f9ef5.firebaseapp.com"; // tomado de oauth_client
+  static const databaseURL =
+      "https://efood-f9ef5-default-rtdb.firebaseio.com"; // tomado de firebase_url
+  static const projectId = "efood-f9ef5"; // tomado de project_id
+  static const storageBucket =
+      "efood-f9ef5.appspot.com"; // tomado de storage_bucket
+  static const messagingSenderId = ""; //no lo vamos a usar
+  static const appId =
+      "1:576033526354:android:b8e006609805980e72c58b"; // tomado de mobilesdk_app_id
+  static const measurementId = ""; //no lo vamos a usar
+}
